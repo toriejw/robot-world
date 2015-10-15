cefbda32f58a3ed6a27317c6c041b656d1894046
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-10-14 21:29:46 -0600

    Fix broken test

--- test/features/user_can_see_individual_robot_pages_test.rb
+++ test/features/user_can_see_individual_robot_pages_test.rb
@@ -4,8 +4,8 @@ class IndividualPageTest < FeatureTest
   def test_user_can_click_on_each_robot
     create_robots(1)
     visit '/robots'
-    # save_and_open_page  
-    click_button '1: name 1'
+    click_link '1: name 1'
+    
     assert_equal '/robots/1', current_path
   end
 end
